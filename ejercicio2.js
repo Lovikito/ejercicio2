f7b5ad8d797e5f7ce15b46f7bacfe666bbec3924
Lovikito <hernandezlusan@gmail.com>
2023-05-07 09:23:43 -0500

    first commit

---
+++ ejercicio2.js
@@ -0,0 +1,32 @@
+// String
+
+const nombre = "Luis Hernandez"
+console.log(nombre)
+
+// Number
+
+edad = 18
+console.log(edad)
+
+// Boolean
+
+eresdesarollador = true
+console.log(eresdesarollador)
+
+// Date
+
+const fechanaci = new Date(2004, 9, 10)
+console.log(fechanaci)
+
+const librofav = {
+    titulo: "Patron Bitcoin",
+    autor: "Saifedean Ammouns",
+    fecha: "2018-9-02",
+    url: "https://www.amazon.es/patr%C3%B3n-Bitcoin-alternativa-descentralizada-centrales/dp/8423429717"
+}
+
+console.log(librofav.titulo)
+console.log(librofav.autor)
+console.log(librofav.fecha)
+console.log(librofav.url)
+
